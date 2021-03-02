17287e5ee3c2eb264cb11dec3ab43b1636fb104c
Chukwudi M. Ngwobia <ngwobiachukwudi@gmail.com>
2021-03-02 23:01:03 +0000

    pair session

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,14 +1,55 @@
 const assert = require('assert').strict;
-const { add, subtract } = require('../');
+const { Calculator } = require('../');
 
-describe('add', () => {
-    it('Should add two numbers correctly.', () => {
-        assert.strictEqual(add(2, 7), 9);
-    });
+
+
+describe('Calculator', () => {
+ it('Should return zero if the input is empty', () => {
+    const calculator = new Calculator();
+    assert.strictEqual(calculator.Sum(), 0);
+ })
+
+ it('Should return the same number if input is only one number', () => {
+     const calculator = new Calculator();
+     assert.strictEqual(calculator.Sum('5'), 5);
+ });
+
+ it('Should return the sume if input is two numbers', () => {
+     const calculator = new Calculator();
+     assert.strictEqual(calculator.Sum('5,12'), 17);
+ });
+
+ it('Should return the sume if input is any amount of numbers', () => {
+     const calculator = new Calculator();
+     assert.strictEqual(calculator.Sum('1,2,3,4'), 10);
+ });
+
+ it('Should return the sum if input is any amount of umbers delimeted by a newline char or comma', () => {
+     const calculator = new Calculator();
+     assert.strictEqual(calculator.Sum('4\n4,4,4\n4'), 20);
+ })
+ 
+ it('Should return the sum if using a custom delimiter', () => {
+     const calculator = new Calculator();
+     assert.strictEqual(calculator.Sum('//.\n1.2.3'), 6);
+ })
+
+ it('Should throw an exception given a negative number as part of the input', () => {
+     const calculator = new Calculator();
+     assert.throws(() => calculator.Sum('1,-2'), new Error('negatives not allowed: -2'));
+ })
 });
 
-describe('subtract', () => {
-    it('Should subtract two numbers correctly.', () => {
-        assert.strictEqual(subtract(9, 7), 2);
-    });
-});
+/*
+1. Given the user input is empty when calculating the sum then it should return zero.
+2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
+3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
+4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
+6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
+7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
+8. Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers). 
+9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
+10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6)
+11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[*][%]\n1*2%3” should return 6)
+*/
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-function add(a, b) {
-    return a + b;
+class Calculator {
+    Sum(input) {
+        if(!input) {
+            return 0;
+        }
+
+        let numbers = []
+        if(input[0] == '/') {
+            const delimeter = input[2];
+            numbers = input.split('\n')[1].split(delimeter).map((element) => parseInt(element, 10));
+        } else {
+            input = input.replace(/\n/g, ',')
+            numbers = input.split(',').map((element) => parseInt(element));
+        }
+
+        const negatives = numbers.filter((element) => element < 0);
+
+        if (negatives.length > 0) {
+            throw new Error(`negatives not allowed: ${negatives[0]}`)
+        }
+
+        return numbers.reduce((prev, current) => {
+            return prev + current;
+        }, 0);
+    }
 }
 
-function subtract(a, b) { /* TODO: implement */ }
-
 module.exports = {
-    add,
-    subtract,
+    Calculator,
 }
